4e5016471d5716b1d984853e4172673d1fe179a0
Théo Bernard <theo.bernard@moonshot-internet.com>
2021-04-15 18:50:36 +0200

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -7,5 +7,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:TheoBernard97/QPUI.git  main:gh-pages
+git push -f git@github.com:TheoBernard97/QPUI.git  master:gh-pages
 cd ..
